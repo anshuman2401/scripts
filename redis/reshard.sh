88c4b0f8aad32ac61553a9b5e66fb27e6e55af53
Anshuman Kaushik <anshumankaushik3@gmail.com>
2021-11-03 11:28:40 +0530

    Create reshard.sh

---
+++ redis/reshard.sh
@@ -0,0 +1,1 @@
+./redis-cli --cluster reshard x.x.x.x:7000 --cluster-from fcc3e533fec7ad4f572d54211ee427514cc7b752 --cluster-to 5c809c01d44c46352c4c0f9ae4404da680f55a86 --cluster-slots 10 --cluster-yes
